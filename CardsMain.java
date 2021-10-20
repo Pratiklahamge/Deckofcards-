e0eeecc5023987cb2c1de292c230a9b7048ceccf
Pratik Lahamge <89896925+Pratiklahamge@users.noreply.github.com>
2021-10-20 20:27:49 +0530

    UC1 Cards

---
+++ CardsMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.workshop2;
+
+public class CardsMain {
+
+	public static void main(String[] args) {
+		DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
+		deckOfCardsGame.welcome();
+		deckOfCardsGame.deckOfCards();
+	}
+
+}
